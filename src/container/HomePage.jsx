d8d458b524b7dc90451cd9eb860a6c52c29bb4e3
MrThapa <thapa.amet27@gmail.com>
2024-03-28 20:31:10 +0545

    added other input boxes

--- src/container/HomePage.jsx
+++ src/container/HomePage.jsx
@@ -10,6 +10,8 @@ const HomePage = () => {
     monthlySalary: "",
     noOfMonths: 12,
     totalSalary: "",
+    bonus: 0,
+    ssf: "",
   };
   const [data, setData] = useState(initialValues);
   const formik = useFormik({
@@ -24,18 +26,24 @@ const HomePage = () => {
   };
   const handleMonthlySalaryChange = (event) => {
     const monthlySalary = event.target.value;
-    const totalSalary = monthlySalary * data.noOfMonths;
+    const totalSalary = monthlySalary * data.noOfMonths + parseInt(data.bonus);
     setData({ ...data, monthlySalary, totalSalary });
   };
   const handleNoOfMonthsChange = (event) => {
     const noOfMonths = event.target.value;
-    const totalSalary = data.monthlySalary * noOfMonths;
+    const totalSalary = data.monthlySalary * noOfMonths + parseInt(data.bonus);
     setData({ ...data, noOfMonths, totalSalary });
+  };
+  const handleBonusChange = (event) => {
+    const bonus = event.target.value;
+    const totalSalary = data.totalSalary + (bonus ? parseInt(bonus) : 0);
+    setData({ ...data, bonus, totalSalary });
   };
   return (
     <div className="container">
       <div className="text-3xl font-bold text-center p-4">
         NEPAL SALARY TAX CALCULATOR
+        <br /> **Under Developemnt**
       </div>
       <form>
         <div className="container-firstHead">
@@ -86,48 +94,105 @@ const HomePage = () => {
         </div>
         <div className="container-secondHead">
           <div>
-            <label htmlFor="monthlySalary" className="container-label">
-              Monthly Salary
-            </label>
-            <input
-              id="monthlySalary"
-              name="monthlySalary"
-              type="number"
-              onChange={(e) => {
-                formik.handleChange(e);
-                handleMonthlySalaryChange(e);
-              }}
-              value={formik.values.monthlySalary}
-            />
+            <div>Yearly Income</div>
+            <div>
+              <label htmlFor="monthlySalary" className="container-label">
+                Monthly Salary
+              </label>
+              <input
+                id="monthlySalary"
+                name="monthlySalary"
+                type="number"
+                onChange={(e) => {
+                  formik.handleChange(e);
+                  handleMonthlySalaryChange(e);
+                }}
+                value={formik.values.monthlySalary}
+              />
+            </div>
+            <div></div>
+            <div>
+              <label htmlFor="totalMonths" className="container-label">
+                No. of Months
+              </label>
+              <input
+                id="totalMonths"
+                name="totalMonths"
+                type="number"
+                onChange={(e) => {
+                  formik.handleChange(e);
+                  handleNoOfMonthsChange(e);
+                }}
+                value={data.noOfMonths}
+              />
+            </div>
+            <div>
+              <label htmlFor="bonus" className="container-label">
+                Bonus
+              </label>
+              <input
+                id="bonus"
+                name="bonus"
+                type="number"
+                onChange={(e) => {
+                  formik.handleChange(e);
+                  handleBonusChange(e);
+                }}
+                value={formik.values.bonus}
+              />
+            </div>
+            <div className="forUnderline"></div>
+            <div>
+              <label htmlFor="totalSalary" className="container-label">
+                Total Salary
+              </label>
+              <input
+                id="totalSalary"
+                name="totalSalary"
+                type="number"
+                value={data.totalSalary}
+                readOnly
+              />
+            </div>
           </div>
-          <div></div>
           <div>
-            <label htmlFor="totalMonths" className="container-label">
-              No. of Months
-            </label>
-            <input
-              id="totalMonths"
-              name="totalMonths"
-              type="number"
-              onChange={(e) => {
-                formik.handleChange(e);
-                handleNoOfMonthsChange(e);
-              }}
-              value={data.noOfMonths}
-            />
+            <div>Yealy Deduction</div>
+            <div>
+              <label htmlFor="ssf" className="container-label">
+                Social Security Fund
+              </label>
+              <input id="ssf" name="ssf" type="number" />
+            </div>
+            <div>
+              <label htmlFor="cit" className="container-label">
+                Citizen Investment Trust
+              </label>
+              <input id="cit" name="cit" type="number" />
+            </div>
+            <div>
+              <label htmlFor="lifeInsurance" className="container-label">
+                Life Insurance
+              </label>
+              <input id="lifeInsurance" name="lifeInsurance" type="number" />
+            </div>
+            <div>
+              <label htmlFor="healthInsurance" className="container-label">
+                Health Insurance
+              </label>
+              <input
+                id="healthInsurance"
+                name="healthInsurance"
+                type="number"
+              />
+            </div>
+            <div>
+              <label htmlFor="houseInsurance" className="container-label">
+                House Insurancs
+              </label>
+              <input id="houseInsurance" name="houseInsurance" type="number" />
+            </div>
           </div>
         </div>
-        <label htmlFor="email" className="container-label">
-          Total Salary
-        </label>
-
-        <input
-          id="totalSalary"
-          name="totalSalary"
-          type="number"
-          value={data.totalSalary}
-          readOnly
-        />
       </form>
     </div>
   );
